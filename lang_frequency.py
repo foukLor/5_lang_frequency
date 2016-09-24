06187dd8830c36ea0c880f7b9119fc5067073574
foukLor <vitaminotar@yandex.com>
2016-09-24 16:54:34 +0300

    script to frequency analysis

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,23 @@
-
+import sys
+from collections import Counter
+import os
+import re
 
 def load_data(filepath):
-    pass
-
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath,'r',encoding = 'utf-8') as file:
+        data = file.read()
+    return data
 
 def get_most_frequent_words(text):
-    pass
+    print(Counter(re.split('\W+',data.lower())).most_common()[:10])
 
 
 if __name__ == '__main__':
-    pass
+    if (len(sys.argv) != 2):
+        exit("Usage:")
+    data  = load_data(sys.argv[1])
+    if not data:
+        exit("Usage:")
+    get_most_frequent_words(data)
